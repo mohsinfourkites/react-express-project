398b84cce74ba9f1485204f5707005b07c871179
mohsinfourkites <mohsin.bhat@fourkites.com>
2024-11-23 15:46:03 +0530

    Signup Login Proper

--- travel-react-frontend/src/components/MyFigma/LoginSignupFigma/AuthContainer/AuthContainer.tsx
+++ travel-react-frontend/src/components/MyFigma/LoginSignupFigma/AuthContainer/AuthContainer.tsx
@@ -3,7 +3,6 @@ import styles from "./Auth.module.scss";
 import LoginFigma from "../LoginFigma/LoginFigma";
 import SignupFigma from "../SignUpFigma/SignUpFigma";
 import { authConstants } from "../authConstants";
-import { LoginForm } from "../../../AceternityUI/LoginForm/LoginForm";
 
 const AuthMain: React.FC = () => {
   const [isLogin, setIsLogin] = useState(false);
@@ -14,57 +13,60 @@ const AuthMain: React.FC = () => {
 
     <div className={styles.whatToShowLoginSignup}>
       {isLogin ? (
-        
-        <div className={styles.mainScreenLogin}>
-          <div className={styles.leftSide}>
+
+        //Signup Screen
+        <div className={styles.signupmainScreenLogin}>
+          <div className={styles.signupleftSide}>
             <SignupFigma toggleView={toggleView} />
 
-            <button className={styles.goToLogintext} onClick={toggleView}>
+            <button className={styles.signupgoToLogintext} onClick={toggleView}>
               Already Have An Account ? <br></br>
               Login
             </button>
           </div>
-          <div className={styles.rightSide}>
-            <div className={styles.rightContainer}>
-              <div className={styles.rightText}>
-                <div className={styles.textheading}>Welcome to Travel</div>
-                <div className={styles.textsubheading}>
+          <div className={styles.signuprightSide}>
+            <div className={styles.signuprightContainer}>
+              <div className={styles.signuprightText}>
+                <div className={styles.signuptextheading}>Welcome To SignUp Page</div>
+                <div className={styles.signuptextsubheading}>
                   <p>Travel with us and explore the world</p>
                 </div>
               </div>
-              <div className={styles.mainImageRight}>
+              <div className={styles.signupmainImageRight}>
                 <img
                   src={authConstants.mainImage}
                   alt="MainImage"
-                  className={styles.mainImageRight}
+                  className={styles.signupmainImageRight}
                 />
               </div>
             </div>
           </div>
         </div>
       ) : (
+
+        //Login Scren
         
-        <div className={styles.mainScreenSignup}>
-          <div className={styles.leftSide}>
+        <div className={styles.loginmainScreenSignup}>
+          <div className={styles.loginleftSide}>
             <LoginFigma toggleView={toggleView} />
-            <button className={styles.goToSignuptext} onClick={toggleView}>
+            <button className={styles.logingoToSignuptext} onClick={toggleView}>
               Don't Have An Account ? <br></br>
               Signup
             </button>
           </div>
-          <div className={styles.rightSide}>
-            <div className={styles.rightContainer}>
-              <div className={styles.rightText}>
-                <div className={styles.textheading}>Welcome to Travel</div>
-                <div className={styles.textsubheading}>
+          <div className={styles.loginrightSide}>
+            <div className={styles.loginrightContainer}>
+              <div className={styles.loginrightText}>
+                <div className={styles.logintextheading}>Welcome to Login Page</div>
+                <div className={styles.logintextsubheading}>
                   <p>Travel with us and explore the world</p>
                 </div>
               </div>
-              <div className={styles.mainImageRight}>
+              <div className={styles.loginmainImageRight}>
                 <img
                   src={authConstants.mainImage}
                   alt="MainImage"
-                  className={styles.mainImageRight}
+                  className={styles.loginmainImageRight}
                 />
               </div>
             </div>
